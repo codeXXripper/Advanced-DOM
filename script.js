e3b21534578c640d717cce0b801701786b0d0567
Israel Fitsum <fafisru@gmail.com>
2023-02-08 14:44:49 +0300

    DOM Traversing

--- script.js
+++ script.js
@@ -221,3 +221,38 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 
 //? Event Delegation
 */
+
+
+const h1 = document.querySelector('h1');
+
+// DOM TRAVERSING
+
+//?Going downwards: child
+
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'blue';
+h1.lastElementChild.style.color = 'orangered';
+
+//?Going upwards: parent
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//? Going sideways: sibling
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
