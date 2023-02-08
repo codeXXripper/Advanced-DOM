da657b70417ab26c4f8bdb96255cbb77f33e2d4d
Israel Fitsum <fafisru@gmail.com>
2023-02-08 17:03:27 +0300

    Tabbed Component

--- script.js
+++ script.js
@@ -89,6 +89,28 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+//Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 ////////////////////////////////////////////////
 /////////////////////////////////////////////////
 /*
@@ -220,7 +242,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 //mdn event.currentTarget
 
 //? Event Delegation
-*/
+
 
 
 const h1 = document.querySelector('h1');
@@ -256,3 +278,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+*/
