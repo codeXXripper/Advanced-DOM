4ecec69addfb8abab06d0dd0503a9a71f66ba86a
Israel Fitsum <fafisru@gmail.com>
2023-02-09 16:59:02 +0300

    passing arguments to event handlers

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+
+const tabs = document.querySelectorAll('.operations__tab');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -91,12 +98,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 //Tabbed component
 
-const tabs = document.querySelectorAll('.operations__tab');
-
-const tabsContainer = document.querySelector('.operations__tab-container');
-
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -104,12 +105,36 @@ tabsContainer.addEventListener('click', function (e) {
 
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate tab
   clicked.classList.add('operations__tab--active');
+
+  //Activate content area
 
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+//Menu fade animation
+
+//Refactoring
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const sibling = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    sibling.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////////////////////
 /////////////////////////////////////////////////
